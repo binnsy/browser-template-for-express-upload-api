5ebd3b832b6290efeb26cc1ca3faaf61e53c66f2
binnsyGA <alice.binns17@gmail.com>
2019-05-10 15:08:37 -0400

    Got an image to console.log in the server, but does not render on the page

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -8,4 +8,12 @@
 
 $(() => {
   // your JS code goes here
+  $('#imageUpload').on('submit', (event) => {
+    event.preventDefault()
+    const formData = new FormData(event.target)
+    for (const [key, value] of formData.entries()) {
+      console.log(key, value)
+    }
+    console.log(formData)
+  })
 })
