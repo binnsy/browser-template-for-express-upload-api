0a92dcde2d132ce9c96bb3e92fa9716c4432f6c2
binnsyGA <alice.binns17@gmail.com>
2019-05-13 12:47:39 -0400

    Modified front end to have a display image div and ajax request

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,14 +6,27 @@
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
+const config = require('./config')
+
 $(() => {
   // your JS code goes here
   $('#imageUpload').on('submit', (event) => {
     event.preventDefault()
     const formData = new FormData(event.target)
-    for (const [key, value] of formData.entries()) {
-      console.log(key, value)
-    }
-    console.log(formData)
+    // for (const [key, value] of formData.entries()) {
+    //   console.log(key, value)
+    $.ajax({
+      url: config.apiUrl + '/uploads',
+      data: formData,
+      cache: false,
+      contentType: false,
+      processData: false,
+      type: 'POST'
+    })
+      .then(data => {
+        console.log(data.upload.url)
+        $('#image-display').append(`<img src=${data.upload.url} />`)
+      })
+      .catch(console.log)
   })
 })
